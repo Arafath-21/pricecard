39d1948aaa99b6b5196cc2fb8df5aeb7b861bd89
arafathyas21@gmail.com <arafathyas21@gmail.com>
2023-12-02 14:34:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from './components/Card'
+function App() {
+  return <>
+        <section className="pricing py-5">
+          <div className="container">
+            <div className="row">
+                <Card />
+            </div>
+          </div>
+        </section>    
+    </>
+}
+
+export default App
